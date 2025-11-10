fe03a5d49337ff9420d380e34c1d34d389631e14
siwanmoon <simon___@naver.com>
2025-11-10 19:47:25 +0900

    feat: Springboot 프로젝트 생성

---
+++ src/test/java/com/siwanmoon/todolist/TodolistApplicationTests.java
@@ -0,0 +1,14 @@
+package com.siwanmoon.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("테스트 통과");
+    }
+
+}
